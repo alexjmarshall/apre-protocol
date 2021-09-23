9dcb3d303fb91f3f1cfa28195e1cb33a97ed86c3
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2021-09-23 15:02:06 -0600

    semicolons

--- load-adjustment.js
+++ load-adjustment.js
@@ -7,14 +7,14 @@ function adjustedLoad(prevLoad, prevReps, protocolKey, increment = 5) {
     (prevLoad) => prevLoad,
     (prevLoad) => prevLoad > 200 ? 10 : 5,
     (prevLoad) => prevLoad > 200 ? 15 : 10
-  ]
+  ];
   const range = (start, stop) => {
     const arr = new Array(stop - start + 1)
     for(let i = 0; i < arr.length; i++){
        arr[i] = start++;
     }
     return arr;
-  }
+  };
   const threeRMRanges = () => [ [1], [2], [3,4], [5,6], range(7,99) ];
   const sixRMRanges = () => [ [1,2], [3,4], range(5,7), range(8,12), range(13,99) ];
   const tenRMRanges = () => [ range(1,6), [7,8], range(9,11), range(12,16), range(17,99) ];
