1575df36c908745625a83548b039d1421f972c35
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2021-09-23 14:55:38 -0600

    Refactor

--- load-adjustment.js
+++ load-adjustment.js
@@ -1,56 +1,39 @@
-function adjustedLoad(prevLoad, prevReps, protocol, increment = 5) {
+function adjustedLoad(prevLoad, prevReps, protocolKey, increment = 5) {
   if(!prevLoad || isNaN(prevLoad) || !prevReps || isNaN(prevReps)) return;
   let loadDiff;
-  switch(protocol) {
-    case "3RM":
-      if(prevReps >= 1 && prevReps <= 2)
-        loadDiff = prevLoad > 200 ? -10 : -5;
-      else if(prevReps >= 3 && prevReps <= 4)
-        loadDiff = 0;
-      else if(prevReps >= 5 && prevReps <= 6)
-        loadDiff = prevLoad > 200 ? 10 : 5;
-      else if(prevReps >= 7)
-        loadDiff = prevLoad > 200 ? 15 : 10;
-      break;
-    case "6RM":
-      if(prevReps >= 1 && prevReps <= 2)
-        loadDiff = prevLoad > 200 ? -10 : -5;
-      else if(prevReps >= 3 && prevReps <= 4)
-        loadDiff = prevLoad > 200 ? -5 : 0;
-      else if(prevReps >= 5 && prevReps <= 7)
-        loadDiff = 0;
-      else if(prevReps >= 8 && prevReps <= 12)
-        loadDiff = prevLoad > 200 ? 10 : 5;
-      else if(prevReps >= 13)
-        loadDiff = prevLoad > 200 ? 15 : 10;
-      break;
-    case "10RM":
-      if(prevReps >= 4 && prevReps <= 6)
-        loadDiff = prevLoad > 200 ? -10 : -5;
-      else if(prevReps >= 7 && prevReps <= 8)
-        loadDiff = prevLoad > 200 ? -5 : 0;
-      else if(prevReps >= 9 && prevReps <= 11)
-        loadDiff = 0;
-      else if(prevReps >= 12 && prevReps <= 16)
-        loadDiff = prevLoad > 200 ? 10 : 5;
-      else if(prevReps >= 17)
-        loadDiff = prevLoad > 200 ? 15 : 10;
-      break;
-    case "15RM":
-      if(prevReps >= 7 && prevReps <= 9)
-        loadDiff = prevLoad > 200 ? -10 : -5;
-      else if(prevReps >= 10 && prevReps <= 12)
-        loadDiff = prevLoad > 200 ? -5 : 0;
-      else if(prevReps >= 13 && prevReps <= 17)
-        loadDiff = 0;
-      else if(prevReps >= 18 && prevReps <= 24)
-        loadDiff = prevLoad > 200 ? 10 : 5;
-      else if(prevReps >= 25)
-        loadDiff = prevLoad > 200 ? 15 : 10;
-      break;
-    default:
-      return;
+  const loadAdjFuncs = [
+    (prevLoad) => prevLoad > 200 ? -10 : -5,
+    (prevLoad) => prevLoad > 200 ? -10 : -5,
+    (prevLoad) => prevLoad,
+    (prevLoad) => prevLoad > 200 ? 10 : 5,
+    (prevLoad) => prevLoad > 200 ? 15 : 10
+  ]
+  const range = (start, stop) => {
+    const arr = new Array(stop - start + 1)
+    for(let i = 0; i < arr.length; i++){
+       arr[i] = start++;
+    }
+    return arr;
   }
+  const threeRMRanges = () => [ [1], [2], [3,4], [5,6], range(7,99) ];
+  const sixRMRanges = () => [ [1,2], [3,4], range(5,7), range(8,12), range(13,99) ];
+  const tenRMRanges = () => [ range(1,6), [7,8], range(9,11), range(12,16), range(17,99) ];
+  const fifteenRMRanges = () => [ range(1,9), range(10,12), range(13,17), range(18,24), range(25,99) ];
+  const protocols = {
+    "3RM": () => new Map(threeRMRanges().map((r,i) => [ r,loadAdjFuncs[i] ])),
+    "6RM": () => new Map(sixRMRanges().map((r,i) => [ r,loadAdjFuncs[i] ])),
+    "10RM": () => new Map(tenRMRanges().map((r,i) => [ r,loadAdjFuncs[i] ])),
+    "15RM": () => new Map(fifteenRMRanges().map((r,i) => [ r,loadAdjFuncs[i] ]))
+  };
+
+ const protocol = protocols[protocolKey]();
+  for(const range of protocol.keys()) {
+    if(range.includes(prevReps)) {
+      loadDiff = protocol.get(range)(prevLoad);
+      break;
+    }
+  }
+
   if(loadDiff % increment) loadDiff = Math.floor(loadDiff / increment) * increment;
   return prevLoad + loadDiff;
 }
